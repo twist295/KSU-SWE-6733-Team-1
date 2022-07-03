899b10c10fa6e07d74141aa03b69b58a6aa6513c
Alvin Abia <alvin.abia@icloud.com>
2022-07-03 09:27:45 -0700

    more typescript scaffolding

--- NextAdventure/src/screens/AuthScreen/AuthScreen.test.tsx
+++ NextAdventure/src/screens/AuthScreen/AuthScreen.test.tsx
@@ -3,6 +3,7 @@ import AuthScreen from './AuthScreen'
 import { Props } from './AuthScreen.type'
 
 type Navigation = Props['navigation']
+type Route = Props['route']
 
 describe('Screens > AuthScreen', () => {
   it('should navigate to sign up on button press', () => {
@@ -12,9 +13,11 @@ describe('Screens > AuthScreen', () => {
       navigate
     }
 
-    const wrapper = shallow(<AuthScreen navigation={navigation as Navigation}/>)
+    const route: Partial<Route> = {};
+
+    const wrapper = shallow(<AuthScreen navigation={navigation as Navigation} route={route as Route} />)
     const signupButton = wrapper.findWhere((w) => w.prop('testID') === 'signup-button').first()
     signupButton.props().onPress()
-    expect(navigate).toHaveBeenCalled()
+    expect(navigate).toHaveBeenCalledWith('Signup')
   })
 })
--- NextAdventure/src/screens/SignupScreen/SignupScreen.tsx
+++ NextAdventure/src/screens/SignupScreen/SignupScreen.tsx
@@ -1,17 +1,33 @@
 import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth'
+import { addDoc, collection, getFirestore } from 'firebase/firestore'
 import { useState } from 'react'
-import { Button, TextInput, View } from 'react-native'
+import { 
+  Alert, Button, TextInput, View } from 'react-native'
 
 const SignupScreen = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
   const signup = async () => {
+    if (email.length === 0 || password.length === 0) {
+      Alert.alert('Error', 'All fields must not be empty')
+      return
+    }
+
     const auth = getAuth()
     try {
-      await createUserWithEmailAndPassword(auth, email, password)
+      const db = getFirestore();
+      const user = await createUserWithEmailAndPassword(auth, email, password)
+      await addDoc(collection(db, 'users'), {
+        uid: user.user.uid
+      })
     } catch (err) {
-      console.log(err)
+      if (err.message.includes('invalid-email')) {
+        Alert.alert('Error', 'Email address is invalid')
+        return
+      }
+
+      Alert.alert('Error', err)
     }
   }
 
