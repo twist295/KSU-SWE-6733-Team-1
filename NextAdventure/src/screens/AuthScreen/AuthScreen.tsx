d31fa65bd3d406e15fb6bdf6a9f061f072438dda
Pollypanda <ploypawachot33@gmail.com>
2022-07-12 14:07:30 -0400

    styling improvements auth page

--- NextAdventure/src/screens/AuthScreen/AuthScreen.tsx
+++ NextAdventure/src/screens/AuthScreen/AuthScreen.tsx
@@ -8,15 +8,35 @@ import {
   StyleSheet,
   Text,
   TextInput, 
-  View 
+  View,
+  Image
+
 } from 'react-native'
 import { Props } from './AuthScreen.type'
 import { sendPWResetEmail, signIn } from '../../utils/Firebase'
+
+import { useFonts } from 'expo-font'
+import AppLoading from 'expo-app-loading'
+
 
 const AuthScreen: FC<Props> = ({ navigation }) => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [isResettingPW, setIsResettingPW] = useState(false)
+
+
+  
+  let [ fontsLoaded ] = useFonts({
+    'Monoton': require('../AuthScreen/assets/fonts/Monoton-Regular.ttf'),
+    'ComingSoon': require('../AuthScreen/assets/fonts/ComingSoon-Regular.ttf'),
+    'Dancing': require('../AuthScreen/assets/fonts/DancingScript-Regular.ttf'),
+    'Ubuntu': require('../AuthScreen/assets/fonts/Ubuntu-Regular.ttf')
+  })
+  if(!fontsLoaded){
+    return <AppLoading/>
+  }
+  
+
 
   const onLogin = async () => {
     try {
@@ -34,15 +54,30 @@ const AuthScreen: FC<Props> = ({ navigation }) => {
     }
   }
 
+  
+  
+
+
   return (
-    <View>
-      <TextInput 
+    <View style = {styles.container}>
+      
+      <View style = {styles.image}>
+      <Image source={require('../AuthScreen/./assets/images.jpeg')}/>
+      </View>
+
+      <Text style = {styles.baseText}>Venture
+      <Text style = {styles.innerText}>Match</Text>
+      </Text>
+
+      <Text style = {styles.smallText}>let's choose your match!</Text>
+      
+      <TextInput style = {styles.email_input}
         onChange={({ nativeEvent }) => setEmail(nativeEvent.text)} 
         placeholder="Email" 
         testID="email-input"
         value={email} />
       {!isResettingPW && (
-        <TextInput 
+        <TextInput style = {styles.password_input}
           onChange={({ nativeEvent }) => setPassword(nativeEvent.text)} 
           placeholder="Password" 
           secureTextEntry 
@@ -50,8 +85,9 @@ const AuthScreen: FC<Props> = ({ navigation }) => {
           value={password} />
       )}
       <Pressable onPress={() => setIsResettingPW(!isResettingPW)} testID="forgot-pw-button">
-        <Text>{isResettingPW ? 'Cancel' : 'Forgot Password' }</Text>
+        <Text style ={styles.forgot_passowrd}>{isResettingPW ? 'Cancel' : 'Forgot Password?' }</Text>
       </Pressable>
+        
       { isResettingPW ? (
         <Button
           disabled={email.length === 0}
@@ -60,7 +96,7 @@ const AuthScreen: FC<Props> = ({ navigation }) => {
           title="Reset Password" />
       ) : (
         <>
-          <Button 
+          <Button
             disabled={email.length === 0 || password.length === 0}
             onPress={onLogin} 
             testID="login-button"
@@ -76,6 +112,82 @@ const AuthScreen: FC<Props> = ({ navigation }) => {
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    //backgroundColor: '#fdfbf4'
+    backgroundColor: '#fbf9ed'
+  },
+
+  image: {
+    //justifyContent: 'center',
+    marginHorizontal: 60,
+    marginVertical: 30,
+  },
+
+  baseText: {
+    fontFamily: 'Monoton',
+    //marginTop: 20,
+    textAlign: 'center',
+    //fontWeight: 'bold',
+    fontSize: 30,
+    color: '#004488',
+    backgroundColor: '#fdfbf4',
+    //padding: 5,
+    
+
+  },
+
+  innerText: {
+    //marginTop: 5,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 30,
+    color: '#EE3377',
+    backgroundColor: '#fdfbf4',
+    //padding: 5,
+  },
+
+  smallText: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 15,
+    color: '#c00000',
+    backgroundColor: '#fdfbf4',
+    //padding: 5,
+    fontFamily: 'Ubuntu',
+
+  },
+  
+  email_input: {
+    marginTop: 40,
+    marginLeft: 20,
+    marginRight: 20,
+    backgroundColor: 'white',
+    padding: 10,
+    borderWidth: 1,
+    borderRadius: 6
+  },
+  password_input: {
+    marginTop: 10,
+    marginLeft: 20,
+    marginRight: 20,
+    backgroundColor: 'white',
+    padding: 10,
+    borderWidth: 1,
+    borderRadius: 6
+  },
+  forgot_passowrd: {
+    marginTop: 5,
+    //marginLeft: 230,
+    //marginRight: 10,
+    //borderRadius: 2,
+    //borderWidth: 0.5,
+    //backgroundColor: '#c4dfe6',
+    textAlign: 'center',
+    fontFamily: 'ComingSoon'
+    
+  }
+
 })
 
 export default AuthScreen;
