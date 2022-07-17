3acce6478308187b314907b6dcca0883584128b7
Aparnareddy1 <99110986+Aparnareddy1@users.noreply.github.com>
2022-07-17 10:03:04 -0400

    Profile screen styling

--- NextAdventure/src/screens/ProfileScreen/ProfileScreen.tsx
+++ NextAdventure/src/screens/ProfileScreen/ProfileScreen.tsx
@@ -1,5 +1,5 @@
 import { launchImageLibraryAsync } from 'expo-image-picker'
-import { 
+import {
   FC,
   useEffect,
   useState
@@ -7,12 +7,12 @@ import {
 import {
   Button,
   FlatList,
-  Image, 
+  Image,
   StyleSheet,
   Text,
   TextInput,
   TouchableOpacity,
-  View 
+  View
 } from 'react-native'
 import { Props } from './ProfileScreen.type'
 import ActivityCell from '../../components/ActivityCell/ActivityCell'
@@ -25,6 +25,7 @@ import {
 } from '../../utils/Firebase'
 import type { Profile } from '../../utils/Type'
 import type { Activity } from '../../utils/Type'
+import { browserPopupRedirectResolver } from 'firebase/auth'
 
 const ProfileScreen: FC<Props> = ({ navigation }) => {
   const [firstName, setFirstName] = useState('')
@@ -42,36 +43,36 @@ const ProfileScreen: FC<Props> = ({ navigation }) => {
 
   const refreshProfile = () => {
     getProfile()
-    .then(profile => {
-      if (!profile) {
-        setIsEditing(true)
-        return
-      }
+      .then(profile => {
+        if (!profile) {
+          setIsEditing(true)
+          return
+        }
 
-      setFirstName(profile.firstName)
-      setLastName(profile.lastName)
-      setFavoriteActivites(profile.favoriteActivities)
-      if (profile.photoURL) {
-        setPictureUrl(profile.photoURL)
-      }
-    })
-    .catch(err => console.log(err))
+        setFirstName(profile.firstName)
+        setLastName(profile.lastName)
+        setFavoriteActivites(profile.favoriteActivities)
+        if (profile.photoURL) {
+          setPictureUrl(profile.photoURL)
+        }
+      })
+      .catch(err => console.log(err))
   }
 
   useEffect(() => {
-    navigation.setOptions({ 
+    navigation.setOptions({
       headerLeft: () => isEditing ? (
         <Button
           color="red"
           title="Cancel"
-          onPress={() => setIsEditing(false)}/>
+          onPress={() => setIsEditing(false)} />
       ) : <></>,
       headerRight: () => isEditing ? (
         <Button
           title="Save"
           onPress={() => save()} />
       ) : (
-        <Button title="Edit" onPress={() => setIsEditing(true)}/>
+        <Button title="Edit" onPress={() => setIsEditing(true)} />
       )
     })
   }, [isEditing])
@@ -112,18 +113,18 @@ const ProfileScreen: FC<Props> = ({ navigation }) => {
   const renderActivity = (activity: Activity, index: number) => {
     const accessory = isEditing ? (
       <View style={styles.editActivityButton}>
-        <Button onPress={() => setEditingActivity(index)} title="Edit"/>
+        <Button onPress={() => setEditingActivity(index)} title="Edit" />
       </View>
     ) : null
 
     return (
-      <ActivityCell activity={activity} accessory={accessory}/>
+      <ActivityCell activity={activity} accessory={accessory} />
     )
   }
 
   const renderProfilePicture = () => {
     if (!pictureUrl) {
-      return <TouchableOpacity disabled={!isEditing} onPress={pickImage} style={styles.emptyPfp}/>
+      return <TouchableOpacity disabled={!isEditing} onPress={pickImage} style={styles.emptyPfp} />
     }
 
     return (
@@ -147,33 +148,33 @@ const ProfileScreen: FC<Props> = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
+      <View style={styles.header}>{renderProfilePicture()}</View>
       <View>
-        {renderProfilePicture()}
-        <Text style={styles.header}>Personal Info</Text>
+        <Text style={styles.name1}>Profile</Text>
         <View style={styles.name}>
           <Text>Name: </Text>
-          { isEditing ? (
-            <TextInput 
+          {isEditing ? (
+            <TextInput
               onChange={({ nativeEvent }) => setFirstName(nativeEvent.text)}
               placeholder="First Name"
               style={styles.nameInput}
               value={firstName} />
-          ) : <Text>{firstName}</Text> }
-          { isEditing ? (
+          ) : <Text>{firstName}</Text>}
+          {isEditing ? (
             <TextInput
-              onChange={({ nativeEvent }) => setLastName(nativeEvent.text)} 
-              placeholder="Last Name" 
+              onChange={({ nativeEvent }) => setLastName(nativeEvent.text)}
+              placeholder="Last Name"
               style={styles.nameInput}
               value={lastName} />
-          ) :  <Text>{lastName}</Text> }
+          ) : <Text>{lastName}</Text>}
         </View>
         {/* { isEditing ? <TextInput placeholder="Location" /> :  <Text></Text> } */}
         <FlatList
-          ListHeaderComponent={<Text style={styles.header}>Favorite Activities</Text>} 
-          ListFooterComponent={isEditing ? (<Button title="Add New" onPress={() => setIsAddingActivity(true)}/>) : null }
+          ListHeaderComponent={<Text style={styles.name}>Favorite Activities</Text>}
+          ListFooterComponent={isEditing ? (<Button title="Add New" onPress={() => setIsAddingActivity(true)} />) : null}
           data={favoriteActivities}
-          renderItem={({ item, index }) => renderActivity(item, index)} 
-          style={styles.favoriteActivities}/>
+          renderItem={({ item, index }) => renderActivity(item, index)}
+          style={styles.favoriteActivities} />
       </View>
       <View style={styles.signout}>
         <Button color="red" title="Sign Out" onPress={signout} />
@@ -181,12 +182,12 @@ const ProfileScreen: FC<Props> = ({ navigation }) => {
       <ActivityModal
         onConfirm={addActivity}
         onDismiss={() => setIsAddingActivity(false)}
-        visible={isAddingActivity}/>
+        visible={isAddingActivity} />
       <ActivityModal
         activity={editingActivity !== -1 ? favoriteActivities[editingActivity] : null}
         onConfirm={updateActivity}
         onDismiss={() => setEditingActivity(-1)}
-        visible={editingActivity != -1}/>
+        visible={editingActivity != -1} />
     </View>
   )
 }
@@ -194,41 +195,71 @@ const ProfileScreen: FC<Props> = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'space-between',
-    padding: 16,
+    backgroundColor: '#fff',
+
+  },
+
+  header: {
+    fontSize: 24,
+    backgroundColor: "purple",
+    height: 200,
+  },
+  pfp: {
+    marginTop: 120,
+    alignSelf: 'center',
+    borderRadius: 50,
+    height: 120,
+    width: 120,
+    borderColor: 'yellow',
+    borderWidth: 4,
+    marginBottom: 10,
+    position: 'absolute'
   },
   editActivityButton: {
-    justifyContent: 'center'
+    justifyContent: 'center',
+    marginRight: 10,
   },
   emptyPfp: {
+    marginTop: 150,
     alignSelf: 'center',
-    backgroundColor: 'gray',
-    borderRadius: 44,
-    height: 88,
-    width: 88
+    borderRadius: 60,
+    height: 120,
+    width: 120,
+    borderColor: 'yellow',
+    borderWidth: 4,
+    backgroundColor: "gray",
+    position: 'absolute'
+  },
+
+  name1: {
+    flexDirection: 'row',
+    fontSize: 25,
+    fontWeight: "600",
+    marginTop: 100
   },
   favoriteActivities: {
-    marginTop: 16
+    marginTop: 20,
+    marginBottom: 20,
+    border: 10,
+
   },
-  header: {
-    fontSize: 24
-  },
+
   name: {
-    flexDirection: 'row'
+    flexDirection: 'row',
+    fontSize: 25,
+    fontWeight: "600",
+    marginTop: 10
+
   },
   nameInput: {
     borderBottomWidth: 1,
     flexGrow: 1,
     marginHorizontal: 8
   },
-  pfp: {
-    alignSelf: 'center',
-    borderRadius: 44,
-    height: 88,
-    width: 88
-  },
+
   signout: {
     borderWidth: 1
+
   }
 })
 
